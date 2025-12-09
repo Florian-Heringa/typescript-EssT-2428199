0955460c32e10c90a18a23b0a1787ca7850c652d
Florian-Heringa <florian_music@hotmail.com>
2025-12-09 14:16:44 +0000

    missed a few exercises

--- exercise_files/Ch02/02_Challenge/app.ts
+++ exercise_files/Ch02/02_Challenge/app.ts
@@ -1,16 +1,29 @@
-const todoItems = [
-    { id: 1, title: "Learn HTML", status: "done", completedOn: new Date("2021-09-11") },
-    { id: 2, title: "Learn TypeScript", status: "in-progress" },
-    { id: 3, title: "Write the best app in the world", status: "todo" },
+enum TodoStatus {
+    Done = 'done',
+    InProgress = 'in-progress',
+    Todo = 'todo'
+}
+
+interface TodoItem {
+    id: number
+    title: string
+    status: TodoStatus,
+    completedOn?: Date
+}
+
+const todoItems: TodoItem[] = [
+    { id: 1, title: "Learn HTML", status: TodoStatus.Done, completedOn: new Date("2021-09-11") },
+    { id: 2, title: "Learn TypeScript", status: TodoStatus.InProgress },
+    { id: 3, title: "Write the best app in the world", status: TodoStatus.Todo },
 ]
 
-function addTodoItem(todo) {
+function addTodoItem(todo: string) : TodoItem {
     const id = getNextId(todoItems)
 
-    const newTodo = {
+    const newTodo : TodoItem = {
         id,
         title: todo,
-        status: "todo",
+        status: TodoStatus.Todo,
     }
 
     todoItems.push(newTodo)
@@ -18,7 +31,11 @@ function addTodoItem(todo) {
     return newTodo
 }
 
-function getNextId(items) {
+interface HasId {
+    id: number
+}
+
+function getNextId<T extends HasId>(items: T[]) : number {
     return items.reduce((max, x) => x.id > max ? x.id : max, 0) + 1
 }
 
